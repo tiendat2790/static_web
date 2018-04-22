4baefa0347d21278a763aaa01a7847fcae221191
Dat <tiendat2790@gmail.com>
2018-04-22 21:39:04 +0700

    Edit Lession 3

--- javascrip/js/lession3.js
+++ javascrip/js/lession3.js
@@ -12,18 +12,24 @@ var a = 20 > 3;
 
 
 function introProfile() {
-    var name = 'Dat';
-    var  age = 26;
-    var job = 'student';
+    var name = 'Đạt';
+    var height = 170;
+    var weight = 60;
+    var age = 25;
+    var job = 'Công Nhân';
+    var wife = 'Giang';
+    var son = 'Shin';
 
-   return 'Tôi ten la ' + name + ' .toi ' + age + ' tuoi. Toi la mot ' + job;
+   return 'Tôi tên là ' + name + ' .Tôi cao ' + height + ' cm. Tôi nặng ' + weight + ' kg. Tôi ' + age + 'tuổi. Hiện tại tôi đang làm ' + job + '. Vợ tôi là ' + wife + '. Con trai tôi là ' + son;
 }
 
-function introProfile2(name, age, job) {
-    return 'Tôi ten la ' + name + ' .toi ' + age + ' tuoi. Toi la mot ' + job;
+function introProfile2(name, height, weight, age, job, wife, son) {
+
+    return 'Tôi tên là ' + name + ' .Tôi cao ' + height + ' cm. Tôi nặng ' + weight + ' kg. Tôi ' + age + 'tuổi. Hiện tại tôi đang làm ' + job + '. Vợ tôi là ' + wife + '. Con trai tôi là ' + son;
 }
 
 var result = introProfile();
-var result = introProfile2('Dat', 26, 'student');
+var result = introProfile2('Đạt', 170, 60, 25, 'Công Nhân', 'Giang', 'Shin')
+
 document.getElementById('my-profile').innerHTML = result;
 document.getElementById('my-profile2').innerHTML = result;
