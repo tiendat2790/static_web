89df879f89bbeb197bf92b70e16b4e12caa56bb7
Dat <tiendat2790@gmail.com>
2018-05-06 00:17:08 +0700

    Lession11

---
+++ javascrip/js/lession10.js
@@ -0,0 +1,26 @@
+//An: 01643160762
+//Hanh: 01656574210
+//Dat: 01676893232
+
+function callYou() {
+    var name = document.getElementById('name').valua;
+    var anPhone = '01643160762';
+    var hanhPhone = '01656574210';
+    var datPhone = '01676893232';
+
+    var currentPhone;
+    switch (name) {
+        case 'An':
+            currentPhone = anPhone;
+            break;
+        case 'Hanh':
+            currentPhone = hanhPhone;
+            break;
+        case 'Dat':
+            currentPhone = datPhone;
+            break;
+    }
+
+    alert(currentPhone);
+
+}
